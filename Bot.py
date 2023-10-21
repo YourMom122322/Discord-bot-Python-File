f9dbd31957b7f80d874b08cb9393962882792c75
Momwhyareyouhere <124384634+YourMom122322@users.noreply.github.com>
2023-10-21 17:12:21 +0200

    Bot.py

--- Bot.py
+++ Bot.py
@@ -1,34 +1,34 @@
 import discord
 from discord.ext import commands
+import random
+from flask import Flask
+import threading
 
-intents = discord.Intents.default()
-intents.message_content = True
+app = Flask(__name__)
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+@app.route('/')
+def keep_alive():
+    return "Keep alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def start():
+    server_thread = threading.Thread(target=run)
+    server_thread.start()
+
+if __name__ == '__main__':
+    start()
+
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return  # Ignore messages from bots
-
-    if message.content.lower() == 'zombie':
-        await message.channel.send('zombie')
-        while True:
-            response = await bot.wait_for('message', check=lambda m: m.author == message.author)
-            if response.content.lower() == 'stop':
-                break
-            else:
-                await message.channel.send('zombie')
-
-    await bot.process_commands(message)
+    print(f'Bot is ready')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('@everyone Pong!')
+    await ctx.send('Ping!')
 
 @bot.command()
 async def hello(ctx):
@@ -36,29 +36,6 @@ async def hello(ctx):
 
 @bot.command()
 async def roll(ctx):
-    import random
-    await ctx.send(f'You rolled a {random.randint(1, 6)}')
+    await ctx.send(random.randint(1, 6))
 
-@bot.command()
-async def clear(ctx):
-    def is_not_server_owner(m):
-        return m.author != ctx.guild.owner
-    
-    await ctx.channel.purge(check=is_not_server_owner)
-    await ctx.send("Messages cleared.", delete_after=5)
-
-@bot.command()
-async def commands(ctx):
-    command_list = [
-        'ping: Responds with "@everyone Pong!"',
-        'hello: Responds with "Hello!"',
-        'roll: Generates a random number between 1 and 6 and sends the result',
-        'clear: Clears messages in the channel, but doesn\'t delete messages from the server owner',
-        'commands: Lists all available commands excluding secret_commands, create_myself_or_die_at_3_am, delete_myself_or_die_at_3_am, DONT_KILL_ME, KILL_ME'
-    ]
-
-    command_text = '\n'.join(command_list)
-    await ctx.send(f'Available commands:\n```{command_text}```')
-
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot.run('YOUR_BOT_TOKEN')
